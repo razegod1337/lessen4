96dd093bd20823158edec81db33c190f15d31ccf
razegod1337 <serzh.zhdanov19@gmail.com>
2023-07-14 15:11:38 +0300

    Main

---
+++ src/BonusMilesService.java
@@ -0,0 +1,12 @@
+public class BonusMilesService {
+    public int calculate(int price) {
+        int result;
+        int bonus = 20;
+        if (price > 0) {
+            result = price / bonus;
+        } else {
+            result = 0;
+        }
+            return result;
+    }
+}
